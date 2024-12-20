0845620b2c07e160accd9f9d8077ceece930902e
unqrf <aid08-abr@yandex.ru>
2024-12-20 10:48:05 +0300

    extract data loading into a function

--- utils/burgers_shock_read.py
+++ utils/burgers_shock_read.py
@@ -2,8 +2,10 @@
 import scipy.io
 import numpy as np
 
-data = scipy.io.loadmat('./burgers_shock.mat')
-t = data['t'].flatten()[:,None] #100*1
-x = data['x'].flatten()[:,None] #256*1
-Exact = np.real(data['usol']).T #100*256
-X,T = np.meshgrid(x, t)
+def load(path='./burgers_shock.mat'):
+    data = scipy.io.loadmat(path)
+    t = data['t'].flatten()[:,None] #100*1
+    x = data['x'].flatten()[:,None] #256*1
+    U = np.real(data['usol']).T #100*256
+    X,T = np.meshgrid(x, t)
+    return X, T, U
